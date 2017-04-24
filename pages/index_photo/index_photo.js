db89da0a898297e6bb659259bcdfdd33f94acd24
qiangqiangbuzhidao <qiangqiangbuzhidao@DESKTOP-I8LFEUJ>
2017-04-24 17:33:03 +0800

    @2017/4/24

---
+++ pages/index_photo/index_photo.js
@@ -0,0 +1,44 @@
+// pages/index_photo/index_photo.js
+Page({
+  data: {
+    hotList: [
+      {
+        pic: '../../img/140px_120px_background.png',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }, {
+        pic: '../../img/140px_120px_background.png',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }, {
+        pic: '../../img/140px_120px_background.png',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }, {
+        pic: '../../img/140px_120px_background.png',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }, {
+        pic: '../../img/140px_120px_background.png',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }
+    ]
+
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
---
+++ pages/index_red_packets/index_red_packets.js
@@ -0,0 +1,84 @@
+// pages/index_red_packets/index_red_packets.js
+
+
+
+Page({
+
+  data: {
+    hotList: [
+      {
+        month: '1月'
+
+      }, {
+        month: '2月'
+
+      }, {
+        month: '3月'
+
+      }, {
+        month: '4月'
+
+      }, {
+        month: '5月'
+
+      }, {
+        month: '6月'
+
+      }, {
+        month: '7月'
+
+      }, {
+        month: '8月'
+
+      }, {
+        month: '9月'
+
+      }, {
+        month: '10月'
+
+      }, {
+        month: '11月'
+
+      }, {
+        month: '12月'
+
+      }, {
+        month: '全部'
+
+      }
+    ],
+    tableList:[
+      {
+        name:'10元',
+        num:'医用',
+        time:'2017-04-20'
+      },{
+        name:'10元',
+        num:'医用',
+        time:'2017-04-20'
+      },{
+        name:'10元',
+        num:'医用',
+        time:'2017-04-20'
+      },
+    ]
+    
+
+
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
